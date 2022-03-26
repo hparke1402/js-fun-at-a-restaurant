4f42d9a7af7913c9c0e3e2a5565ae42c23482ac2
Hannah Parker <hparke1402@gmail.com>
2022-03-25 22:56:40 -0400

    Adding code for unit testing functions

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,56 @@
-function nameMenuItem() {
-
+function nameMenuItem(food) {
+ return `Delicious ${food}`;
 }
+
+function createMenuItem(itemName, itemPrice, itemType) {
+  menuItem = {
+    name: itemName,
+    price: itemPrice,
+    type: itemType,
+  };
+  return menuItem;
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+}
+
+function decreasePrice(decreasedPrice) {
+  return menuItem.price * .90;
+}
+
+
+
+
+
+function addIngredients(ingredientToAdd, ingredients) {
+if (ingredients.includes(ingredientToAdd)) {
+  return ingredients;
+} else {
+  ingredients.push(ingredientToAdd)
+  return ingredients;
+}
+}
+
+
+
+function createRecipe(title, ingredients, menuItemType) {
+  return recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+}
+
+
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
