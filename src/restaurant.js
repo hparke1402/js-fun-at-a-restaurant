51459af4613a17b2d1accc430327bc282ccaf83d
Hannah Parker <hparke1402@gmail.com>
2022-03-27 20:28:07 -0400

    Adding functions for 10/13 unit tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,40 @@
+function createRestaurant(restaurantName) {
+  return {
+    name: restaurantName,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+}
+
+function addMenuItem(restaurantName, foodItem) {
+  var menuToAddTo = restaurantName.menus[foodItem.type];
+
+for (i = 0; i < menuToAddTo.length; i++) {
+
+  if (menuToAddTo[i] === foodItem) {
+    menuToAddTo.splice(i, 1);
+
+    }
+
+}
+menuToAddTo.push(foodItem);
+}
+
+
+//taking in restaurant object and food item ,
+//
+// push method array
+
+function removeMenuItem() {
+
+}
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
